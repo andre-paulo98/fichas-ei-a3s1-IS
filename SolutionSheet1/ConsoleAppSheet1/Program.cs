f94249887aa2ab7ee9b98568acb4b70d153c6c74
André Paulo <andre_duivenvoorden@hotmail.com>
2020-10-01 11:41:52 +0100

    ficha 1 e 2
    com gitignore aplicado

---
+++ SolutionSheet1/ConsoleAppSheet1/Program.cs
@@ -0,0 +1,18 @@
+﻿using OrganizationLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSheet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Integração de Sistemas");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SolutionSheet1/OrganizationLib/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizationLib
+{
+    public abstract class Employee : Person
+    {
+
+        private double _salary;
+
+        public virtual double Salary { get { return _salary; } }
+
+        protected Employee(string firstname, string lastname, DateTime birthdate, double salary ) : base(firstname, lastname, birthdate)
+        {
+            _salary = salary;
+        }
+
+        public override string Print()
+        {
+            StringBuilder st = new StringBuilder();
+
+            st.AppendLine("-------------------------");
+            st.AppendLine($"Nome Completo: {Fullname}");
+            st.AppendLine($"Idade: {Age}");
+            st.AppendLine($"Salario: {Salary}");
+            st.AppendLine("-------------------------");
+
+            return base.Print();
+        }
+    }
+}
